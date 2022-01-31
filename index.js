b4d6a869cd59e1265394de474fc36a4d5adc2893
sivastu <you@example.com>
2022-01-31 10:34:18 +0530

    'poimopmdpobm'

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-const express = require('express')
-const serve = express()
+const http = require('http')
 
-const apps =()=>{
-	console.log('not worked')
-	serve.listen(3006,()=>{
-		console.log('Server up and running 3006')
-	})
-}
-
-const geet=()=>{
-	console.log('worked')
-	serve.get('/one',(req,res)=>{
-		res.status(200).json({ message: "here is nedkjnenefyour public resource" });
-	})
-}
-
-
-serve.get('/',(req,res)=>{
-	res.status(200).json({ message: "public resource" });
+const server = http.createServer((req,res)=>{
+	res.end()
 })
 
-module.exports = {
-   apps,
-   geet
-}
+server.listen('8000','localhost',()=>{
+	console.log('worked')
+})
